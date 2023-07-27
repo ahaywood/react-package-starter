173e92dfcb141f54489df2a65b1f85648162c5f4
Amy Haywood Dutton <me@amyhaywood.com>
2023-07-27 17:19:15 -0500

    ✨ Added Tailwind configurations

---
+++ src/tailwind/blurs.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  theme: {
+    extend: {
+      blur: {
+        8: '4px',
+        16: '8px',
+        24: '12px',
+        32: '16px',
+        40: '20px',
+      }
+    }
+  }
+}
---
+++ src/tailwind/typography.config.js
@@ -0,0 +1,74 @@
+module.exports = {
+  theme: {
+    extend: {
+      fontFamily: {
+        'sans': ['Inter', 'sans-serif'],
+      },
+      fontSize: {
+        display112: ['7rem', {
+          letterSpacing: '-0.02em',
+          lineHeight: '7.25rem'
+        }],
+        display80: ['5rem', {
+          letterSpacing: '-0.02em',
+          lineHeight: '5.25rem'
+        }],
+        display60: ['3.75rem', {
+          letterSpacing: '-0.02em',
+          lineHeight: '4.25rem'
+        }],
+        headline48: ['3rem', {
+          letterSpacing: '-0.01em',
+          lineHeight: '3.5rem',
+        }],
+        headline36: ['2.25rem', {
+          letterSpacing: '-0.01em',
+          lineHeight: '2.75rem',
+        }],
+        headline28: ['1.75rem', {
+          letterSpacing: '-0.01em',
+          lineHeight: '2rem',
+        }],
+        headline24: ['1.5rem', {
+          letterSpacing: '-0.01em',
+          lineHeight: '2rem',
+        }],
+        title22: ['1.375rem', {
+          lineHeight: '1.75rem',
+        }],
+        title16: ['1rem', {
+          lineHeight: '1.25rem',
+        }],
+        title12: ['0.75rem', {
+          lineHeight: '1rem',
+        }],
+        body20: ['1.25rem', {
+          lineHeight: '2rem'
+        }],
+        body18: ['1.125rem', {
+          lineHeight: '1.5rem'
+        }],
+        body16: ['1rem', {
+          lineHeight: '1.5rem'
+        }],
+        body14: ['0.875rem', {
+          lineHeight: '1.25rem'
+        }],
+        body12: ['0.75rem', {
+          lineHeight: '1rem'
+        }],
+        label18: ['1.125rem', {
+          lineHeight: '1.75rem'
+        }],
+        label16: ['1rem', {
+          lineHeight: '1.25rem',
+          letterSpacing: '0.002em'
+        }],
+        label14: ['0.875rem', {
+          letterSpacing: '0.005em',
+          lineHeight: '1.25rem'
+        }],
+      },
+    }
+  }
+}
