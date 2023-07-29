06ef80940dabaef3ed5bffcb7e6072250b52000f
Amy Haywood Dutton <me@amyhaywood.com>
2023-07-28 23:39:53 -0500

    🔥 Removed Cabana elements

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,5 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  presets: [
-    require('./src/tailwind/colors.config.js'),
-    require('./src/tailwind/typography.config.js'),
-    require('./src/tailwind/shadows.config.js'),
-    require('./src/tailwind/blurs.config.js'),
-    require('./src/tailwind/utilities.config.js'),
-  ],
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
     extend: {},
